e32c794458bb1736c0e8705ed3097dfcf3209aed
rangoljx <rangoljx@qq.com>
2024-12-16 09:32:26 +0800

    autocommit:  - 2024-12-16

--- push-mkdocs.bat
+++ push-mkdocs.bat
@@ -1,7 +1,7 @@
 @REM @echo off
 cd /d "D:\VSCODE\rangoljx.github.io"
 set commitDate=%date:~0,4%-%date:~5,2%-%date:~8,2%
-set /p commitMessage="请输入提交信息: "
+set /p commitMessage="Please enter the submit info:"
 git add .
 git commit -m "autocommit: %commitMessage% - %commitDate%"
 @REM git commit -m "auto-commit - %commitDate%"

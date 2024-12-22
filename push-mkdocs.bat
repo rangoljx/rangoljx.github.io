b912b3899a858b894cdff48ce094a3d7cc837be9
rangoljx <rangoljx@qq.com>
2024-12-22 18:00:25 +0800

    autocommit: Update page layout - 2024-12-22

--- push-mkdocs.bat
+++ push-mkdocs.bat
@@ -5,5 +5,5 @@ set /p commitMessage="Please enter the submit info:"
 git add .
 git commit -m "autocommit: %commitMessage% - %commitDate%"
 @REM git commit -m "auto-commit - %commitDate%"
-git push -u origin master
-@REM git push -u origin master --force
+@REM git push -u origin master
+git push -u origin master --force
